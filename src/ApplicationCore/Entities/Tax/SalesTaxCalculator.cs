b59a9c2d61544ab1ad6106146b17805c27ae4bef
Shyam Namboodiripad <gnamboo@microsoft.com>
2024-05-14 11:12:46 -0700

    [Demo] Code for 'fix with copilot' demo for //build

---
+++ src/ApplicationCore/Entities/Tax/SalesTaxCalculator.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.eShopWeb.ApplicationCore.Entities.Tax;
+
+public class SalesTaxCalculator
+{
+    public virtual decimal GetSalesTax(decimal total, string city, string state)
+    {
+        var taxRate = StateAndLocalTaxHelper.GetTaxRate(city, state);
+        return total * taxRate / 100;
+    }
+}
